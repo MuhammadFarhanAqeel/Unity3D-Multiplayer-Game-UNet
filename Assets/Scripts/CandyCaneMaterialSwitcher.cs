609729ae00eff2efcb80e4cb0d26a476f70cf63c
Muhammad-Farhan <farhan.aqeel@hotmail.com>
2018-11-26 12:21:19 +0500

    FPS implementation.
    First person controller done with Lan network connection.

---
+++ Assets/Scripts/CandyCaneMaterialSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyCaneMaterialSwitcher : MonoBehaviour {
+
+
+
+	public Material localCaneMaterial;
+	public Material notLocalCaneMaterial;
+
+	Renderer candyCaneRenderer;
+
+
+
+
+	// Use this for initialization
+	void Awake () {
+
+		candyCaneRenderer = GetComponent<Renderer> ();
+
+
+		SwitchMaterial (false);
+	}
+	
+	void SwitchMaterial(bool isLocalPlayer){
+		if (isLocalPlayer) {
+			candyCaneRenderer.material = localCaneMaterial;
+		} else {
+			candyCaneRenderer.material = notLocalCaneMaterial;
+		}
+	}
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour {
+
+
+	public ParticleSystem _muzzleFlash;
+	public AudioSource _gunAudio;
+	public GameObject _impactPrefab;
+	public Transform cameraTransform;
+
+	ParticleSystem _impactEffect;
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		_impactEffect = Instantiate (_impactPrefab).GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1")) {
+		
+			_muzzleFlash.Stop ();
+			_muzzleFlash.Play();
+
+			_gunAudio.Stop ();
+			_gunAudio.Play ();
+
+
+			RaycastHit hit;
+			Vector3 rayPos = cameraTransform.position + (1f * cameraTransform.forward);
+
+			if (Physics.Raycast (rayPos, cameraTransform.forward, out hit, 50f)) {
+			
+				_impactEffect.transform.position = hit.point;
+				_impactEffect.Stop ();
+				_impactEffect.Play ();
+
+
+				if (hit.transform.tag == "Player") {
+				
+					Destroy (hit.transform.gameObject);
+
+				}
+
+			}
+
+		}
+		
+	}
+}
