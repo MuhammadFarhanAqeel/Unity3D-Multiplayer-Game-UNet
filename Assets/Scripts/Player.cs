4908c672d2370fb23d4146dcdfbb7a1897bc0435
Muhammad-Farhan <farhan.aqeel@hotmail.com>
2019-01-31 13:05:42 +0500

    Updated networking SDK
    Updated the networking sdk to impvove code compatibility.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool> {
+}
+
+
+
+public class Player : NetworkBehaviour {
+
+     
+    [SerializeField] ToggleEvent onToggleShared;
+    [SerializeField] ToggleEvent onToggleLocal;
+    [SerializeField] ToggleEvent onToggleRemote;
+
+    GameObject MainCamera; 
+    void Start() {
+        MainCamera = Camera.main.gameObject;
+        EnablePlayer();
+
+
+    }
+
+
+
+    void DisablePlayer() {
+        if (isLocalPlayer)
+            MainCamera.SetActive(true);
+
+        onToggleShared.Invoke(false);
+
+        if (isLocalPlayer)
+        {
+            onToggleLocal.Invoke(false);
+        }
+        else
+        {
+            onToggleRemote.Invoke(false);
+        }
+    }
+
+    void EnablePlayer() {
+        if (isLocalPlayer)
+            MainCamera.SetActive(false);
+
+        onToggleShared.Invoke(true);
+
+        if (isLocalPlayer)
+        {
+            onToggleLocal.Invoke(true);
+        }
+        else {
+            onToggleRemote.Invoke(true);
+        }
+    }
+
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Unity3D-Multiplayer-Game-UNet
